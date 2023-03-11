35f1fb1baa2edaedcebb9944cb306471cfc47205
Alex Torres <32007930+aimarket@users.noreply.github.com>
2023-03-11 15:56:09 -0700

    updated test file code

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ sudo useradd $name
 sudo passwd -d $name
 uid=$(id -u $name)
 make clean
-make
+make all
 echo "Test script will start $num processes for the user $uid"
 sudo su $name -c ./process_gen/process_generator &
 sleep 10
@@ -16,10 +16,11 @@ let OUTPUT
 OUTPUT=$PS_SCRIPT; 
 echo "Test script will insert the test kernel module"
 sudo insmod producer_consumer.ko uid=$uid buff_size=$2 p=$3 c=$4 && 
+stty sane
 echo "The test script waits 10 seconds for the kernel module to finish."
 sleep 10
 echo "Test script will remove the test kernel module"
 sudo rmmod producer_consumer; 
 echo "The following is the dmesg output"
-dmesg | tail -n $5; echo $OUTPUT; 
+sudo dmesg | tail -n $5; echo $OUTPUT; 
 sudo pkill -u $uid; rm -rf var;
